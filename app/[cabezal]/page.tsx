f8c86478450341e8fa28143dd9727514bcb54eaa
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2023-10-30 17:35:08 -0300

    estableciendo parametros estaticos

--- app/[cabezal]/page.tsx
+++ app/[cabezal]/page.tsx
@@ -1,3 +1,16 @@
-export default function Cabezal({ params }: { params: { cabezal: string } }) {
-  return <div className="text-2xl font-bold m-4">{params.cabezal}</div>
+export async function generateStaticParams() {
+  return [
+    { cabezal: 'cabezal1' },
+    { cabezal: 'cabezal2' },
+    { cabezal: 'cabezal3' },
+    { cabezal: 'cabezal4' },
+    { cabezal: 'cabezal5' },
+  ]
+}
+
+export const dynamicParams = false
+
+export default function Page({ params }: { params: { cabezal: string } }) {
+  const { cabezal } = params
+  return <div className="text-4xl font-bold">{cabezal}</div>
 }
