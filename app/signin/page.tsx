3b3143156b2a9dfbca1d59c6407906a73bbe7e23
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2023-10-30 18:03:40 -0300

    añadiendo componente inicio de sesión

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,13 +1,26 @@
 'use client'
 
 import { Switch, Card, Text } from '@tremor/react'
+import { useState } from 'react'
 
 export default function Signin() {
+  const [isSwitchOn, setIsSwitchOn] = useState<boolean>(false)
+
+  const handleSwitchChange = (value: boolean) => {
+    setIsSwitchOn(value)
+  }
+
   return (
     <main className="h-screen flex align-middle">
       <Card className="w-fit m-auto align-middle">
-        <Text>Iniciar Sesión</Text>
-        <Switch />
+        <label htmlFor="switch">Iniciar Sesión</label>
+        <Switch
+          id="switch"
+          checked={isSwitchOn}
+          onChange={handleSwitchChange}
+          error={isSwitchOn}
+          errorMessage="Error"
+        />
       </Card>
     </main>
   )
