3f77979c996c8cf43c5be78ab89e6310100e048e
cristian-boaventura <96512124+cristian-boaventura@users.noreply.github.com>
2023-11-01 17:57:47 -0300

    rearreglando useeffect

--- components/CabezalesTable.tsx
+++ components/CabezalesTable.tsx
@@ -52,7 +52,7 @@ export default function CabezalesTable() {
       }
     }
     getCabezales()
-  })
+  }, [getToken])
 
   const isCabezalSelected = (cabezal: Cabezal) =>
     selectedNames.includes(cabezal.name) || selectedNames.length === 0
